763c612438f9345b8f5c04ba30786728234eb222
leixu <qa-jenkins@google.com>
2014-03-25 18:28:48 +0800

    refine UI fitting for mobile

--- js/index.js
+++ js/index.js
@@ -1,11 +1,46 @@
 $(document).ready(function($) {
+	setPanelWidth();
     setLeft();
 	loadBigImage();
 	hoverBar();
 	hoverLogo();
 });
 
+$(window).resize(function() {
+	if ($(window).width() < 800) {
+		$(".right_side").css({
+			'left': 0,
+			'right': ''
+		});
+		$(".article").css({
+			'left': 0,
+			'right': 0,
+			'width': "95%"
+		});
+		$(".side_bar").css({
+			'display': 'none'
+		});
+	} else {
+		$(".right_side").css({
+			'left': '',
+			'right': 0
+		});
+		$(".article").css({
+			'left': '',
+			'right': '',
+			'width': '650px'
+		});
+		$(".side_bar").css({
+			'display': 'block'
+		});
+	}
+});
 
+function setPanelWidth() {
+	var width = $(window).width()/2;
+	$(".left_pic").width(width);
+	$(".right_side").width(width);
+}
 
 function hoverBar() {
 	var left_distance = ($(window).width() - 650) / 2;
@@ -44,3 +79,10 @@ function hoverLogo(){
 		$(this).css('background-color','gray');
 	});
 }
+
+function collectArticleByMouth() {
+	$(".posts li").each(function() {
+		var date = $(this).find('span').html();
+	});
+}
+
