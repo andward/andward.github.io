33b4807160d61da5981149a8be0e7da716ae2016
Lei Xu <leixu@leixu-macbookair.roam.corp.google.com>
2015-02-10 11:42:20 +0800

    create demo

--- demo/demo.js
+++ demo/demo.js
@@ -4,6 +4,9 @@ var pageInit = {
 	init: function(settings) {
 		pageInit.config = {
 			mainbody: $(".mainbody"),
+			painting: $(".painting"),
+			abstract: $(".abstract"),
+			scrollSpeed: 500
 		};
 		$.extend(pageInit, settings);
 
@@ -11,6 +14,31 @@ var pageInit = {
 	},
 
 	fullpage: function() {
-		pageInit.config.mainbody.fullpage();
+		pageInit.config.mainbody.fullpage({
+			scrollingSpeed: pageInit.config.scrollSpeed,
+			onLeave: function(index, direction) {
+				if (index == 1) {
+					pageInit.config.painting
+					.find(".text")
+					.animate({
+						'top': '-=50px'
+					}, 1000);
+					pageInit.config.abstract
+					.find('.text')
+					.css('top', 0);
+				} else if (index == 2) {
+					pageInit.config.abstract
+					.find('.text')
+					.animate({
+						'top': '-=50px'
+					}, 1000);
+					pageInit.config.painting
+					.find(".text")
+					.css("top", 0);
+				}
+			}
+		});
 	}
 };
+
+$(document).ready(pageInit.init);
